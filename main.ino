40f16e04d59456028900ca6aa534408c5dfb58af
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-08-20 14:12:57 -0400

    Implement button press handling and display function
    Added functions to handle button press and display text.

--- main.ino
+++ main.ino
@@ -16,9 +16,7 @@ int targetTime;
 float player1Time;
 float player2Time;
 
-
 void setup() {
-
   pinMode(button1, INPUT_PULLUP);
   pinMode(button2, INPUT_PULLUP);
 
@@ -26,10 +24,8 @@ void setup() {
   randomSeed(analogRead(A0));
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
-
   display.setTextSize(2);
   display.setCursor(15, 20);
   display.println("TIMER GAME");
@@ -41,4 +37,37 @@ void setup() {
 
 void loop(){
   
+}
+
+void waitForPress(int button) {
+  while (digitalRead(button) == HIGH) {
+    delay(10);
+  }
+
+  //deb
+  delay(50);
+  while (digitalRead(button) == LOW) {
+      delay(10);
+    }
+  delay(50);
+}
+
+
+void showScreen(String line1, String line2, String line3) {
+  display.clearDisplay();
+  
+  display.setTextColor(SSD1306_WHITE);
+
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println(line1);
+  
+  display.setTextSize(1);
+  display.setCursor(0, 28);
+  display.println(line2);
+
+  display.setCursor(0, 48);
+  display.println(line3);
+
+  display.display();
 }
