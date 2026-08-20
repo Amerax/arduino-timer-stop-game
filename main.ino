d7cde176aa751cfe08dded08f046de124a4f61e0
Raiyan K <96327738+Amerax@users.noreply.github.com>
2026-08-20 15:15:09 -0400

    Implement player timing and win condition logic

--- main.ino
+++ main.ino
@@ -36,7 +36,100 @@ void setup() {
 }
 
 void loop(){
-  
+  targetTime = random(1, 6);
+
+  showScreen(
+    "PLAYER 1",
+    "TARGET: " + String(targetTime) + " SEC",
+    "PRESS TO START"
+  );
+
+  waitForPress(button1);
+
+  showScreen(
+    "PLAYER 1",
+    "GO!",
+    "PRESS TO STOP"
+  );
+
+  unsigned long startTime = millis();
+  waitForPress(button1);
+  unsigned long endTime = millis();
+  player1Time = (endTime - startTime) / 1000.0;
+
+  showScreen(
+    "PLAYER 1",
+    String(player1Time, 2) + " SEC",
+    "DONE!"
+  );
+
+  delay(2000);
+
+  showScreen(
+    "PLAYER 2",
+    "TARGET: " + String(targetTime) + " SEC",
+    "PRESS TO START"
+  );
+
+  waitForPress(button2);
+
+  showScreen(
+    "PLAYER 2",
+    "GO!",
+    "PRESS TO STOP"
+  );
+
+  startTime = millis();
+  waitForPress(button2);
+  endTime = millis();
+  player2Time = (endTime - startTime) / 1000.0;
+
+  showScreen("PLAYER 2",String(player2Time, 2) + " SEC","DONE!" );
+  delay(2000);
+
+  float difference1 = abs(player1Time - targetTime);
+  float difference2 = abs(player2Time - targetTime);
+
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+
+  display.setCursor(0, 0);
+  display.print("TARGET: ");
+  display.print(targetTime);
+  display.println(" SEC");
+
+  display.setCursor(0, 15);
+  display.print("P1: ");
+  display.print(player1Time, 2);
+  display.println(" SEC");
+
+  display.setCursor(0, 28);
+  display.print("P2: ");
+  display.print(player2Time, 2);
+  display.println(" SEC");
+
+
+  display.setTextSize(2);
+  display.setCursor(0, 45);
+
+  if (difference1 < difference2) {
+
+    display.println("P1 WINS!");
+
+  } 
+  else if (difference2 < difference1) {
+
+    display.println("P2 WINS!");
+  } 
+  else {
+
+    display.println("TIE!");
+  }
+
+  display.display();
+
+  delay(5000);
 }
 
 void waitForPress(int button) {
